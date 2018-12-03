ef00d75249617f93f9039e216e674c630c1dad86
Alex Boicu <a.boicu@levi9.com>
2018-12-03 09:52:54 +0200

    Navigation bar and login card

--- src/app/maerial-custom.module.ts
+++ src/app/maerial-custom.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule,
+    MatCardModule, MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule, MatCardModule,
+        MatProgressSpinnerModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule, MatCardModule,
+        MatProgressSpinnerModule],
 })
 export class MaterialCustomModule { }
