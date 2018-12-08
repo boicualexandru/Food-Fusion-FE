11e737fd8a67a836221041a8eddc0241d2740af7
Alex Boicu <boicualexandru@mail.com>
2018-12-08 21:55:44 +0200

    AuthService (work in progress)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { LoginModel } from '../models/loginModel';
 import { UserState } from '../models/userState';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
     private apiPath = 'http://localhost:56416/api';
 
-    private token: string;
+    private tokenSubject: BehaviorSubject<string>;
+    private userStateSubject: BehaviorSubject<UserState>;
 
-    constructor(private http: HttpClient) { }
+    public get currentUser(): Observable<UserState> {
+        return this.userStateSubject.asObservable();
+    }
+
+    public get httpOptions(): any {
+        return {
+            headers: new HttpHeaders({
+              'Content-Type':  'application/json',
+              'Authorization': 'Bearer ' + this.tokenSubject.value
+            })
+          };
+    }
+
+    constructor(private http: HttpClient) {
+        this.tokenSubject = new BehaviorSubject<string>(localStorage.getItem('jwt'));
+
+        const userState: UserState = this.parseToken(this.tokenSubject.value);
+        this.userStateSubject = new BehaviorSubject<UserState>(userState);
+    }
 
     login(loginModel: LoginModel) {
         console.log('AuthService login');
         this.http.post(this.apiPath + '/Token', loginModel, { responseType: 'text' })
             .subscribe(
                 (token: string) => {
-                    this.token = token;
-
-                    const helper = new JwtHelperService();
-                    const decodedToken = helper.decodeToken(token);
-
-                    const userState: UserState = {
-                        token: token,
-                        expirationDate: helper.getTokenExpirationDate(token),
-                        userId: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
-                        email: decodedToken['email'],
-                        fullName: decodedToken['full_name'],
-                        managedRestaurants: decodedToken['managed_restaurant'],
-                        employeeOfRestaurants: decodedToken['employee_of_restaurant']
-                    };
+                    const userState: UserState = this.parseToken(token);
 
                     localStorage.setItem('jwt', token);
-                    localStorage.setItem('userState', JSON.stringify(userState));
+
+                    this.tokenSubject.next(token);
+                    this.userStateSubject.next(userState);
+
                     console.log(token);
                     console.log(userState);
                 },
@@ -43,6 +53,28 @@ export class AuthService {
 
     logout() {
         localStorage.removeItem('jwt');
-        localStorage.removeItem('userState');
+        this.tokenSubject.next(null);
+        this.userStateSubject.next(null);
+    }
+
+    private parseToken(token: string): UserState {
+        if (token == null) {
+            return null;
+        }
+
+        const helper = new JwtHelperService();
+        const decodedToken = helper.decodeToken(token);
+
+        const userState: UserState = {
+            token: token,
+            expirationDate: helper.getTokenExpirationDate(token),
+            userId: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
+            email: decodedToken['email'],
+            fullName: decodedToken['full_name'],
+            managedRestaurants: decodedToken['managed_restaurant'],
+            employeeOfRestaurants: decodedToken['employee_of_restaurant']
+        };
+
+        return userState;
     }
 }
