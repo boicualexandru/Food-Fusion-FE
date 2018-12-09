d7128ffb46469d04622b3d40d3c574b795e84ebf
Alex Boicu <boicualexandru@mail.com>
2018-12-09 19:57:58 +0200

    Restaurant page tabs

--- src/app/maerial-custom.module.ts
+++ src/app/maerial-custom.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule,
-    MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatDividerModule } from '@angular/material';
+    MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatDividerModule, MatTabsModule } from '@angular/material';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule, MatCardModule,
-        MatProgressSpinnerModule, MatMenuModule, MatDividerModule],
+        MatProgressSpinnerModule, MatMenuModule, MatDividerModule, MatTabsModule],
     exports: [MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule, MatCardModule,
-        MatProgressSpinnerModule, MatMenuModule, MatDividerModule],
+        MatProgressSpinnerModule, MatMenuModule, MatDividerModule, MatTabsModule],
 })
 export class MaterialCustomModule { }
