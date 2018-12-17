b7ee8e48e7fe386e903b4b31b21407c69a2b6c9b
Alex Boicu <boicualexandru@mail.com>
2018-12-17 22:53:01 +0200

    Remove log from serverSideValidator

--- src/app/helpers/serverSideValidator.ts
+++ src/app/helpers/serverSideValidator.ts
@@ -3,8 +3,6 @@ import { BehaviorSubject } from 'rxjs';
 
 export function serverSideValidator(serverErrorMessage: BehaviorSubject<any>, fieldName: string = ''): ValidatorFn {
     return (control: AbstractControl): {[key: string]: any} | null => {
-        console.log(serverErrorMessage.value);
-
         if (serverErrorMessage.value == null) { return; }
         const isInvalid = serverErrorMessage.value[fieldName] != null;
         return isInvalid ? {'serverSideValidator': {value: serverErrorMessage.value[fieldName]}} : null;
