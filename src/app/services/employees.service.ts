3c49aab536e119061f366fc5c2e1aa89ec50a0b2
Alex Boicu <a.boicu@levi9.com>
2018-12-17 15:48:40 +0200

    Remove Employee Service method

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -19,24 +19,12 @@ export class EmployeesService {
     }
 
     addEmployeeByEmail(restaurantId: number, email: string) {
-        return this.http.post(this.apiPath + '/Restaurants/' + restaurantId + '/Employees', JSON.stringify(email),
+        return this.http.post<Employee>(this.apiPath + '/Restaurants/' + restaurantId + '/Employees', JSON.stringify(email),
             {headers: {'Content-Type': 'application/json'}});
-            // .pipe(catchError(this.handleError));
     }
 
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-          // A client-side or network error occurred. Handle it accordingly.
-          console.error('An error occurred:', error.error.message);
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong,
-          console.error(
-            `Backend returned code ${error.status}, ` +
-            `body was: ${error.error}`);
-        }
-        // return an observable with a user-facing error message
-        return throwError(
-          'Something bad happened; please try again later.');
-      }
+    removeEmployee(restaurantId: number, userId: number) {
+        return this.http.delete<void>(this.apiPath + '/Restaurants/' + restaurantId + '/Employees/' + userId,
+            {headers: {'Content-Type': 'application/json'}});
+    }
 }
