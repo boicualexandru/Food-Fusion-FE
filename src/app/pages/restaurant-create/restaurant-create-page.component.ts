36e7a3c78a0aa65edf1d8db85b8f489a5ea70a52
Alex Boicu <a.boicu@levi9.com>
2018-12-17 17:55:31 +0200

    Create Restaurant page (work in progress)

---
+++ src/app/pages/restaurant-create/restaurant-create-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './restaurant-create-page.component.html',
+    styleUrls: ['./restaurant-create-page.component.scss']
+})
+export class RestaurantCreatePageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
