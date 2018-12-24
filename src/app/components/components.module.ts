f740af64e904eb37025dfaa35885a5ade7506fe7
Alex Boicu <boicualexandru@mail.com>
2018-12-24 16:04:13 +0200

    Add app-availability-slider that extends mat-slider

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared.module';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { AvailabilitySliderComponent } from './availability-slider/availability-slider.component';
 
 @NgModule({
-    declarations: [ ConfirmDialogComponent ],
+    declarations: [ ConfirmDialogComponent, AvailabilitySliderComponent ],
     imports: [ SharedModule ],
-    exports: [],
+    exports: [ AvailabilitySliderComponent ],
     providers: [],
     entryComponents: [ ConfirmDialogComponent ]
 })
