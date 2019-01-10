75d58fba2da3a45c45650fb6c30aad02ede2566c
Alex Boicu <a.boicu@levi9.com>
2019-01-10 18:04:03 +0200

    Decorator

---
+++ src/app/services/decorators.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { MenuItem } from '../models/menu/menuItem';
+
+export interface IDecoratedItem {
+    getPrice(): number;
+}
+
+export class DecoratedItem implements IDecoratedComponent {
+    private item: MenuItem;
+
+    constructor(item: MenuItem) {
+        this.item = item;
+    }
+
+    getPrice(): number {
+        return this.item.price;
+    }
+}
+
+export interface IDecorator extends IDecoratedItem {
+    baseItem: IDecoratedItem;
+}
+
+export class TomatoSauceDecorator implements IDecorator {
+    baseItem: IDecoratedItem;
+    private price = 2;
+
+    constructor(baseItem: IDecoratedItem) {
+        this.baseItem = baseItem;
+    }
+
+    getPrice(): number {
+        return this.baseItem.getPrice() + this.price;
+    }
+}
+
+export class SpicySauceDecorator implements IDecorator {
+    baseItem: IDecoratedItem;
+    private price = 3;
+
+    constructor(baseItem: IDecoratedItem) {
+        this.baseItem = baseItem;
+    }
+
+    getPrice(): number {
+        return this.baseItem.getPrice() + this.price;
+    }
+}
+
+@Injectable()
+export class DecoratorsService {
+
+}
