e64a3bdedfc8c810e0736cfef097970daa54b5dc
Alex Boicu <a.boicu@levi9.com>
2019-03-18 12:07:16 +0200

    changed port for backend api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://34.245.155.215/api',
+  apiBaseUrl: 'http://34.245.155.215:5000/api',
   apiDateFormatMinutePrecision: 'yyyy-MM-ddTHH:mm'
 };
