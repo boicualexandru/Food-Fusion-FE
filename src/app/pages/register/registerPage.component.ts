da00a668f842607283d59dfc3d66fdd13ff70560
Alex Boicu <boicualexandru@mail.com>
2019-05-06 00:05:17 +0300

    Register page validation

--- src/app/pages/register/registerPage.component.ts
+++ src/app/pages/register/registerPage.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { RegisterModel } from 'src/app/models/registerModel';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BasicErrorStateMatcher } from 'src/app/helpers/basicErrorStateMatcher';
 
 @Component({
     templateUrl: './registerPage.component.html',
@@ -12,9 +14,21 @@ export class RegisterPageComponent implements OnInit {
     showSpinner = false;
     showError = false;
 
-    email: string;
-    fullName: string;
-    password: string;
+    matcher = new BasicErrorStateMatcher();
+
+    form = new FormGroup({
+        email: new FormControl('', [
+            Validators.required,
+            Validators.email
+        ]),
+        fullName: new FormControl('', [
+            Validators.required
+        ]),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+        ])
+    });
 
     constructor(private router: Router, private authService: AuthService) { }
 
@@ -23,9 +37,9 @@ export class RegisterPageComponent implements OnInit {
     register(): void {
         this.showSpinner = true;
         const registerModel: RegisterModel = {
-            email: this.email,
-            fullName: this.fullName,
-            password: this.password
+            email: this.form.value.email,
+            fullName: this.form.value.fullName,
+            password: this.form.value.password
         };
 
         this.authService.register(registerModel).subscribe(
