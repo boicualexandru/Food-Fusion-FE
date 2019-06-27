ea1dba9a325cc30d838eced93cc6c938947b6220
Alex Boicu <boicualexandru@mail.com>
2019-06-27 22:54:40 +0300

    Using the api retrieved restaurant images to display on retsaurants list

--- src/app/pages/restaurants/restaurantsPage.component.ts
+++ src/app/pages/restaurants/restaurantsPage.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantsService } from 'src/app/services/restaurants.service';
 import { Restaurant } from 'src/app/models/restaurant/restaurant';
-import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './restaurantsPage.component.html',
@@ -17,11 +16,5 @@ export class RestaurantsPageComponent implements OnInit {
 
     ngOnInit(): void {
         this.restaurantsService.getAll().subscribe(restaurants => this.restaurants = restaurants);
-    }
-
-    getImage(): string {
-        const d = Math.random();
-
-        return 'https://source.unsplash.com/800x600/?dinner,food&a=' + d.toString();
     }
 }
