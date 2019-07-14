ff124860741f915d152e20ffcc1fbc3e33e4c6d7
Alex Boicu <boicualexandru@mail.com>
2019-07-14 18:38:35 +0300

    Restaurants map info windows

--- src/app/pages/restaurants/restaurantsPage.component.ts
+++ src/app/pages/restaurants/restaurantsPage.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, Inject, ViewChildren, QueryList } from '@angular/core';
 import { RestaurantsService } from 'src/app/services/restaurants.service';
 import { Restaurant } from 'src/app/models/restaurant/restaurant';
+import { AgmInfoWindow, AgmMarker } from '@agm/core';
 
 @Component({
     templateUrl: './restaurantsPage.component.html',
@@ -8,14 +9,41 @@ import { Restaurant } from 'src/app/models/restaurant/restaurant';
 })
 export class RestaurantsPageComponent implements OnInit {
     restaurants: Restaurant[] = [];
+    focusedRestaurantId?: number = null;
     clientHeight: number;
     mapView = true;
 
-    constructor(private restaurantsService: RestaurantsService) {
+    defaultMarkerUrl = 'assets/img/map-marker-default.png';
+    accentMarkerUrl = 'assets/img/map-marker-accent.png';
+
+    @ViewChildren('infoWindow') infoWindows: QueryList<AgmInfoWindow>;
+
+    constructor(
+        private restaurantsService: RestaurantsService,
+        @Inject(ChangeDetectorRef) private changeDetectorRef: ChangeDetectorRef) {
         this.clientHeight = window.innerHeight;
     }
 
     ngOnInit(): void {
-        this.restaurantsService.getAll().subscribe(restaurants => this.restaurants = restaurants);
+        this.restaurantsService.getAll().subscribe(restaurants => {
+            this.restaurants = restaurants;
+        });
+    }
+
+    cardMouseEnter(restaurantId: number) {
+        this.focusedRestaurantId = restaurantId;
+    }
+
+    cardMouseLeave(restaurantId: number) {
+        this.focusedRestaurantId = null;
+    }
+
+    isFocused(restaurantId: number): boolean {
+        return this.focusedRestaurantId === restaurantId;
+    }
+
+    clearInfoWindows(): void {
+        this.changeDetectorRef.detectChanges();
+        this.infoWindows.forEach(element => element.close());
     }
 }
